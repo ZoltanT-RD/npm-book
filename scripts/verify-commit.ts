e3557f504ce75ba7ef5fad09109bbdd88d293d8d
ZoltanT-RD <zoltan@resdiary.com>
2024-06-12 13:43:51 +0100

    feat(core): chapter 3

---
+++ scripts/verify-commit.ts
@@ -0,0 +1,13 @@
+/*const message = process.argv[2].trim();
+console.log("message below:");
+console.log(message);
+
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(\(.+\))?: .{1,50}/;
+
+if (!commitRE.test(message)) {
+  console.log();
+  console.error(`invalid commit message format.`);
+  process.exit(1);
+}
+*/
